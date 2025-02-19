5016f63e616c9be8232efcfd7a1c6d6351caf4da
ryandvl <ryandvl@proton.me>
2025-02-19 20:25:43 -0300

    Lesson 02

--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,4 +1,6 @@
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import z from "zod";
+import { subscribeToEvent } from "../functions/subscribe-to-event";
 
 export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
   app.post(
@@ -7,10 +9,28 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
       schema: {
         summary: "Subscribes someone to the event",
         tags: ["subscription"],
+        body: z.object({
+          name: z.string(),
+          email: z.string().email()
+        }),
+        response: {
+          201: z.object({
+            subscriberId: z.string(),
+          })
+        }
       },
     },
     async (request, reply) => {
-      return reply.status(201);
+      const { name, email } = request.body
+
+      const { subscriberId } = await subscribeToEvent({
+        name,
+        email
+      })
+
+      return reply.status(201).send({
+        subscriberId
+      })
     }
   );
 };
